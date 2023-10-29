b05332be76c8f66e7128ea2f71e55645116a1132
vanadium23 <chernoffivan@gmail.com>
2023-10-29 19:50:41 +0400

    ref: one more try on prompt

--- internal/tagging/openai.go
+++ internal/tagging/openai.go
@@ -70,15 +70,15 @@ func (tagger OpenaiTagger) GuessTags(title, content string) ([]string, error) {
 }
 
 const taggingPrompt = `
-Your task is to analyze the title and content of a given article, primarily from the fields of technology, management, or science, and generate a valid JSON array containing three hierarchical tags. Follow these guidelines for the tags:
-1. The first tag pinpoints the article's most specific aspect.
-2. The second tag broadens the scope slightly.
-3. The third tag denotes the widest category that encompasses the article. It must be one of the following phrases: programming, software engineering, infrastructure, management, science, business, productivity, fun.
-4. Avoid using words directly from the article's title or content, except for those universally recognized within the domain or part of established ontologies.
-5. All tags must be in English, even for articles originally in Russian or any other language.
-6. All tags must be in lowercase.
+Your task is to analyze the title and content of a given article and generate a valid JSON array containing with three hierarchical values. Follow these guidelines for the values:
+1. All values must be in English, even for articles originally in Russian or any other language.
+2. All values must be in lowercase.
+3. All values must be one or two words long.
+4. One value must be from the following list: programming, software engineering, infrastructure, management, science, business, productivity, fun.
+5. One value must be from the following list: python, golang, django, rust, javascript, bash, testing, refactoring, software architecture, system design, microservices, api, event driven architecture, monolith, cloud, docker, nginx, k8s, database, postgresql, shell, monitoring, devops, product management, project management, communication, documentation, leadership, agile, estimates, practices, hiring, decision making, system thinking, history, physics, quantum computing, career, startup, finance, time management, note taking, writing, obsidian, learning, videogame, boardgame, book, rant.
+6. Avoid using words directly from the article's title or content, except for those universally recognized within the domain or part of established ontologies.
 
-Ensure the output is a valid JSON array of strings, not an array of objects. Each string is a single tag.
+Ensure the output is a valid JSON array of strings, not an array of objects.
 Example Correct Response Format:
-["performance", "measurements", "technology"]
+["software engineering", "system design", "python"]
 `
